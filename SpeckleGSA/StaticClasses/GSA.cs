2e16ee768f598a64e5749cad8a2b98d54ad5fcdf
Nicolaas Burgers <nic.burgers@arup.com>
2019-06-25 10:24:07 +1000

    Switched from interop DLL to GSA.tlb for build 44.

--- SpeckleGSA/StaticClasses/GSA.cs
+++ SpeckleGSA/StaticClasses/GSA.cs
@@ -6,9 +6,9 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Interop.Gsa_10_0;
 using SQLite;
 using SpeckleCore;
+using Gsa_10_0;
 
 namespace SpeckleGSA
 {
--- SpeckleGSA/StaticClasses/HelperFunctions.cs
+++ SpeckleGSA/StaticClasses/HelperFunctions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Interop.Gsa_10_0;
+using Gsa_10_0;
 using System.Text.RegularExpressions;
 using System.Windows.Media.Media3D;
 using System.Drawing;
