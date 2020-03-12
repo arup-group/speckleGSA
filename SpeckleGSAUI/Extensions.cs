aa597f638a02b447c05c1f7b398fc9d2a791abf2
Nicolaas Burgers <nic.burgers@arup.com>
2020-03-12 18:18:13 +1100

    Made very crude changes to make some user actions in the UI a little more responsive.  It now also allows users to open files first, then login.

---
+++ SpeckleGSAUI/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SpeckleGSAUI
+{
+  public static class Extensions
+  {
+
+    public static void DoEvents(this Application a)
+    {
+      a.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate { }));
+    }
+  }
+}
