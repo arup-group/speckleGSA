42a4165d0412aa8749d0ccf6bf0b3572366d815e
Nicolaas Burgers <nic.burgers@arup.com>
2021-07-26 20:45:17 +1000

    Tweaked the maximum payload and timeout to better suit the current state of the servers and fixed some other minor issues.

--- SpeckleGSAUI/ViewModels/Commands.cs
+++ SpeckleGSAUI/ViewModels/Commands.cs
@@ -224,52 +224,42 @@ namespace SpeckleGSAUI.ViewModels
       GSA.App.LocalSettings.Result1DNumPosition = coordinator.SenderTab.AdditionalPositionsFor1dElements; //end points (2) plus additional
 
       var resultsToSend = coordinator.SenderTab.ResultSettings.ResultSettingItems.Where(rsi => rsi.Selected).ToList();
-      if (resultsToSend == null || resultsToSend.Count() == 0 || string.IsNullOrEmpty(coordinator.SenderTab.LoadCaseList))
+      if (resultsToSend != null && resultsToSend.Count() > 0 && !string.IsNullOrEmpty(coordinator.SenderTab.LoadCaseList))
       {
-        return false;
+
+        //Prepare the cache for the ability to parse the load case string
+        var initialData = GSA.App.Proxy.GetGwaData(GSA.App.LocalCache.KeywordsForLoadCaseExpansion, false);
+        for (int i = 0; i < initialData.Count(); i++)
+        {
+          var applicationId = (string.IsNullOrEmpty(initialData[i].ApplicationId)) ? null : initialData[i].ApplicationId;
+          GSA.App.Cache.Upsert(
+            initialData[i].Keyword,
+            initialData[i].Index,
+            initialData[i].GwaWithoutSet,
+            streamId: initialData[i].StreamId,
+            applicationId: applicationId,
+            gwaSetCommandType: initialData[i].GwaSetType);
+        }
+
+        var resultCases = GSA.App.LocalCache.ExpandLoadCasesAndCombinations(coordinator.SenderTab.LoadCaseList);
+        if (resultCases == null || resultCases.Count() == 0)
+        {
+          return false;
+        }
+        percentageProgress.Report(5);
+
+        loggingProgress.Report(new MessageEventArgs(MessageIntent.Display, MessageLevel.Information, "Resolved load cases"));
+
+        GSA.App.Settings.ResultCases = resultCases;
+        GSA.App.Settings.ResultTypes = resultsToSend.Select(rts => rts.ResultType).ToList();
+
+        if (GSA.App.LocalSettings.SendResults && resultCases.Count() > 0)
+        {
+          GSA.App.LocalProxy.PrepareResults(GSA.App.Settings.ResultTypes, GSA.App.LocalSettings.Result1DNumPosition + 2);
+
+          loggingProgress.Report(new MessageEventArgs(MessageIntent.Display, MessageLevel.Information, "Extracted results"));
+        }
       }
-
-      //Prepare the cache for the ability to parse the load case string
-      var initialData = GSA.App.Proxy.GetGwaData(GSA.App.LocalCache.KeywordsForLoadCaseExpansion, false);
-      for (int i = 0; i < initialData.Count(); i++)
-      {
-        var applicationId = (string.IsNullOrEmpty(initialData[i].ApplicationId)) ? null : initialData[i].ApplicationId;
-        GSA.App.Cache.Upsert(
-          initialData[i].Keyword,
-          initialData[i].Index,
-          initialData[i].GwaWithoutSet,
-          streamId: initialData[i].StreamId,
-          applicationId: applicationId,
-          gwaSetCommandType: initialData[i].GwaSetType);
-      }
-
-      var resultCases = GSA.App.LocalCache.ExpandLoadCasesAndCombinations(coordinator.SenderTab.LoadCaseList);
-      if (resultCases == null || resultCases.Count() == 0)
-      {
-        return false;
-      }
-      percentageProgress.Report(5);
-
-      loggingProgress.Report(new MessageEventArgs(MessageIntent.Display, MessageLevel.Information, "Resolved load cases"));
-
-      GSA.App.Settings.ResultCases = resultCases;
-
-      var selectedResultNames = resultsToSend.Select(rts => rts.Name).ToList();
-      GSA.App.Settings.ResultTypes = resultsToSend.Select(rts => rts.ResultType).ToList();
-      /*
-      GSA.App.Settings.NodalResults = ExtractResultParams(ref Result.NodalResultMap, selectedResultNames);
-      GSA.App.Settings.Element1DResults = ExtractResultParams(ref Result.Element1DResultMap, selectedResultNames);
-      GSA.App.Settings.Element2DResults = ExtractResultParams(ref Result.Element2DResultMap, selectedResultNames);
-      GSA.App.Settings.MiscResults = ExtractResultParams(ref Result.MiscResultMap, selectedResultNames);
-      */
-
-      if (GSA.App.LocalSettings.SendResults && resultCases.Count() > 0)
-      {
-        //GSA.App.LocalProxy.PrepareResults(GSA.App.LocalSettings.Result1DNumPosition + 2, selectedResultNames, resultCases, percentageProgress);
-        GSA.App.LocalProxy.PrepareResults(GSA.App.Settings.ResultTypes, GSA.App.LocalSettings.Result1DNumPosition + 2);
-      }
-
-      loggingProgress.Report(new MessageEventArgs(MessageIntent.Display, MessageLevel.Information, "Extracted results"));
 
       coordinator.SenderTab.SetDocumentName(GSA.App.Proxy.GetTitle());
 
